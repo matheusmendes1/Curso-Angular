44ca7d8fa9cd75cc856a183f2225e3a7ba16e450
Matheus Santos <matheusmendes1@hotmail.com>
2019-12-24 09:04:04 -0300

    End Chapter 1

---
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { PlatformDetectorService } from 'src/app/core/plataform-detector/platform-detector.service';
+
+@Component({
+    templateUrl: './signin.component.html'
+})
+export class SignInComponent implements OnInit{
+
+    loginForm: FormGroup;
+    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
+
+    constructor(
+        private formBuider: FormBuilder, 
+        private authService: AuthService,
+        private router: Router,
+        private platformDetectorService: PlatformDetectorService 
+    ){ }
+
+    ngOnInit(): void{
+        this.loginForm = this.formBuider.group({
+            userName: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+
+    login() {
+
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+    
+        this.authService
+            .authenticate(userName, password)
+            .subscribe(
+                () => this.router.navigate(['user', userName]),
+                err => {
+                    console.log(err);
+                    this.loginForm.reset();
+                    this.userNameInput.nativeElement.focus()
+                    alert('invalid user name or password');
+                }
+            );
+    }
+}
---
+++ alurapic/src/app/shared/components/vmessage/vmessage.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { VMessageComponent } from './vmessage.component';
+
+@NgModule({
+    declarations: [ VMessageComponent ],
+    exports: [ VMessageComponent ]
+})
+export class VMessageModule{
+
+    
+}
