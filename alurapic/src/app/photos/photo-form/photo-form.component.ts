250935776b7c9dfab6c197b5b735acd3b0ca6eec
Matheus Santos <matheusmendes1@hotmail.com>
2019-12-26 22:17:04 -0300

    End Chapter 3 - Part 3 Course

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PhotoService } from '../photo/photo.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'ap-photo-form',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoFormComponent implements OnInit {
 
-  constructor() { }
+  photoForm: FormGroup;
+  file: File;
+  preview: string;
+
+  constructor(
+    private photoService: PhotoService, 
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.photoForm = this.formBuilder.group({
+      file: [
+        '',
+        Validators.required
+      ],
+      description: [
+        '',
+        Validators.maxLength(300)
+      ],
+      allowComments: [true]
+    })
   }
 
+  upload(){
+    const description = this.photoForm.get('description').value;
+    const allowComments = this.photoForm.get('allowComments').value;
+    
+    this.photoService
+      .upload(description, allowComments, this.file)
+      .subscribe(() => this.router.navigate(['']));
+  }
+
+  handleFile(file: File){
+    this.file = file;
+    
+    const reader = new FileReader();
+    reader.onload = (event: any) => this.preview = event.target.result;
+    reader.readAsDataURL(file);
+  }
+  
 }
